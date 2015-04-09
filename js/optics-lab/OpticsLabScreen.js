cc5bd5d420c2358a9bdfe2b931e3c86cf87fd3f8
Chris Malley <cmalley@pixelzoom.com>
2015-04-09 11:16:22 -0600

    remove extra space from copyright comment on line 1 of .js files

--- js/optics-lab/OpticsLabScreen.js
+++ js/optics-lab/OpticsLabScreen.js
@@ -1,4 +1,4 @@
-//  Copyright 2002-2014, University of Colorado Boulder
+// Copyright 2002-2014, University of Colorado Boulder
 
 /**
  * The 'Optics Lab' screen. Conforms to the contract specified in joist/Screen.
