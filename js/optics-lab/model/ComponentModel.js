86f8a6c286384a4c61ffb3bf6ad33140c6312f04
dubson <dubson21356@gmail.com>
2015-06-28 11:35:32 -0600

    Minor code clean-up in Trig-Lab. Starting Optics-Lab.

---
+++ js/optics-lab/model/ComponentModel.js
@@ -0,0 +1,22 @@
+/**
+ * Created by Duso on 6/28/2015.
+ */
+
+define( function( require ) {
+    'use strict';
+
+    // modules
+    var inherit = require( 'PHET_CORE/inherit' );
+    var Vector2 = require( 'DOT/Vector2' );
+
+    function ComponentModel( diameter, focalLength ) {
+        this.position = new Vector2( 0, 0 );
+        this.
+        var fLength
+        var components = [];
+    }
+
+    return inherit(Object, OpticsLabModel, {
+        }
+    );
+} );
--- js/optics-lab/model/OpticsLabModel.js
+++ js/optics-lab/model/OpticsLabModel.js
@@ -12,7 +12,25 @@ define( function( require ) {
   var inherit = require( 'PHET_CORE/inherit' );
 
   function OpticsLabModel() {
+    var sources = [];
+    var components = [];
   }
 
-  return inherit( Object, OpticsLabModel );
+  return inherit(Object, OpticsLabModel, {
+        addSource: function (source) {
+          sources.push(source);
+        },
+        addComponent: function (component) {
+          components.push(component);
+        },
+        removeSource: function (source) {
+          var index = sources.indexOf(source);
+          sources.splice(index, 1);
+        },
+        removeComponent: function (component) {
+          var index = components.indexOf( component );
+          components.splice( index, 1 );
+        }
+      }
+  );
 } );
