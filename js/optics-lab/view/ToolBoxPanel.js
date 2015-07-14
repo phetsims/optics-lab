451caf73902ffc2a3642ece7c2a273f08fb7fd51
dubson <michael.dubson@colorado.edu>
2015-07-14 12:45:03 -0600

    Working on ToolBoxPanel

--- js/optics-lab/view/ToolBoxPanel.js
+++ js/optics-lab/view/ToolBoxPanel.js
@@ -10,12 +10,19 @@ define( function( require ) {
 
   // modules
   var inherit = require( 'PHET_CORE/inherit' );
-  var Line = require( 'KITE/segments/Line' );
-  //var PropertySet = require( 'AXON/PropertySet' );
+  var VBox = require( 'SCENERY/nodes/VBox' );
+  var HBox = require( 'SCENERY/nodes/HBox' );
+  var HStrut = require( 'SCENERY/nodes/HStrut' );
+  var CheckBox = require( 'SUN/CheckBox' );
+  var Text = require( 'SCENERY/nodes/Text' );
+  var PhetFont = require( 'SCENERY_PHET/PhetFont' );
+  var Node = require( 'SCENERY/nodes/Node' );
+  var Bounds2 = require( 'DOT/Bounds2' );
+  var Rectangle = require( 'SCENERY/nodes/Rectangle' );
+  var Path = require( 'SCENERY/nodes/Path' );
+  var SimpleDragHandler = require( 'SCENERY/input/SimpleDragHandler' );
+  var Vector2 = require( 'DOT/Vector2' );
   var Shape = require( 'KITE/Shape' );
-  //
-  // var Ray2 = require( 'DOT/Ray2' );
-  //var Vector2 = require( 'DOT/Vector2' );
 
   /**
    * {vector2] startDir = direction of starting ray
@@ -35,7 +42,7 @@ define( function( require ) {
     Panel.call( this )
   }//end constructor
 
-  return inherit( Object, RayPath, {
+  return inherit( Panel, RayPath, {
       clearPath: function() {
 
       }
