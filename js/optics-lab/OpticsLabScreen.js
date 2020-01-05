381461b6c3cfa05e2827294fda1715ec67a02ab4
phet-dev <phetadmin@gmail.com>
2020-01-05 03:23:00 -0700

    update copyright dates from daily grunt work

--- js/optics-lab/OpticsLabScreen.js
+++ js/optics-lab/OpticsLabScreen.js
@@ -1,4 +1,4 @@
-// Copyright 2014-2019, University of Colorado Boulder
+// Copyright 2014-2020, University of Colorado Boulder
 
 /**
  * The 'Optics Lab' screen. Conforms to the contract specified in joist/Screen.
