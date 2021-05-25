8147ab0720c1e276cc52a090e2e394bced7dabd2
phet-dev <phetadmin@gmail.com>
2021-05-25 16:00:02 -0600

    update copyright dates from daily grunt work

--- js/optics-lab/view/SourceNode.js
+++ js/optics-lab/view/SourceNode.js
@@ -1,4 +1,4 @@
-// Copyright 2015-2020, University of Colorado Boulder
+// Copyright 2015-2021, University of Colorado Boulder
 
 /**
  * Node for Source of light, which is either a fan of rays (point source)
