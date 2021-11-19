89e8b754fc240f66d876d6e8994850ef78872241
Sam Reid <reids@colorado.edu>
2021-11-18 17:44:07 -0700

    Run grunt update, see https://github.com/phetsims/chipper/issues/1148

--- js/opticsLabStrings.ts
+++ js/opticsLabStrings.ts
@@ -3,11 +3,17 @@
 /**
  * Auto-generated from modulify, DO NOT manually modify.
  */
-
+/* eslint-disable */
 import getStringModule from '../../chipper/js/getStringModule.js';
 import opticsLab from './opticsLab.js';
 
-const opticsLabStrings = getStringModule( 'OPTICS_LAB' );
+type StringsType = {
+  'optics-lab': {
+    'title': string
+  }
+};
+
+const opticsLabStrings = getStringModule( 'OPTICS_LAB' ) as StringsType;
 
 opticsLab.register( 'opticsLabStrings', opticsLabStrings );
 
