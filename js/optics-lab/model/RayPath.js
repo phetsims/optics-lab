9375074c88d77e65095a8cbf291b299d725fdc41
phet-dev <phetadmin@gmail.com>
2022-03-05 03:26:49 -0700

    update copyright dates from daily grunt work

--- js/optics-lab/model/RayPath.js
+++ js/optics-lab/model/RayPath.js
@@ -1,4 +1,4 @@
-// Copyright 2015-2020, University of Colorado Boulder
+// Copyright 2015-2022, University of Colorado Boulder
 
 /**
  * A RayPath is an array of line segments representing the path of a ray of light
