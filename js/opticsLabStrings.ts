20304534768c32afff37e239ef9a8e7cceac0734
Jonathan Olson <jonathan.olson@colorado.edu>
2022-08-15 20:36:40 -0600

    Grunt update, and changes for string Properties, see https://github.com/phetsims/chipper/issues/1302

--- js/opticsLabStrings.ts
+++ js/opticsLabStrings.ts
@@ -5,11 +5,13 @@
  */
 /* eslint-disable */
 import getStringModule from '../../chipper/js/getStringModule.js';
+import TReadOnlyProperty from '../../axon/js/TReadOnlyProperty.js';
 import opticsLab from './opticsLab.js';
 
 type StringsType = {
   'optics-lab': {
     'title': string;
+    'titleProperty': TReadOnlyProperty<string>;
   }
 };
 
