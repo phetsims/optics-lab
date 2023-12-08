7c789b09fd9ba3bf54b0d0c774bd5eacf641e490
Marla Schulz <marlaa.schulz@gmail.com>
2023-12-08 14:23:50 -0700

    Document custom layout bounds across multiple screen views to prevent future breaks in phet-io instrumentation, https://github.com/phetsims/phet-io/issues/1939.

--- js/optics-lab/view/OpticsLabScreenView.js
+++ js/optics-lab/view/OpticsLabScreenView.js
@@ -28,6 +28,9 @@ class OpticsLabScreenView extends ScreenView {
    */
   constructor( opticsLabModel ) {
 
+    // A PhET wide decision was made to not update custom layout bounds even if they do not match the
+    // default layout bounds in ScreenView. Do not change these bounds as changes could break or disturb
+    // any phet-io instrumention. https://github.com/phetsims/phet-io/issues/1939
     super( { layoutBounds: new Bounds2( 0, 0, 768, 504 ) } );
 
     this.mainModel = opticsLabModel;
